fcbf798d0b3b790b37e7f8588addb212e6a0573a
viankahmercado <angelviankahmercado@gmail.com>
2024-01-25 00:37:56 +0800

    pseudocode

--- exercise_1.py
+++ exercise_1.py
@@ -2,3 +2,9 @@
 
 # Exercise 1: Calculate the multiplication and sum of two numbers
 # Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+
+#function to calculate sum or product
+#if-else statement for the condition
+#assign variables for given 1 and given 2
+#print the results
